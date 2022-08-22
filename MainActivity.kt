4a59acd03ea4ab89a52c279cc91fe39e93abef4f
Mücahit Yılmaz <82089578+pessimisticdeveloper@users.noreply.github.com>
2022-08-22 23:59:47 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -20,9 +20,21 @@ class MainActivity : AppCompatActivity() {
             val vize:Int=Integer.parseInt(vizenotu.text.toString())
             val final:Int=Integer.parseInt(finalnotu.text.toString())
             val isonuc = (vize*40)/100 + (final*60)/100
-            sonuc.text=isonuc.toString()
-        }
-
-
-    }
-}
+   if (isonuc <= 44) {
+                sonuc.setText(isonuc.toString() + "\n" + "Harf Notu: FF")
+            } else if (45 <= isonuc && isonuc <= 54) {
+                sonuc.setText(isonuc.toString() + "\n" + "Harf Notu: DE")
+            } else if (55 <= isonuc && isonuc <= 59) {
+                sonuc.setText(isonuc.toString() + "\n" + "Harf Notu: DD")
+            } else if (60 <= isonuc && isonuc <= 69) {
+                sonuc.setText(isonuc.toString() + "\n" + "Harf Notu: CC")
+            } else if (70 <= isonuc && isonuc <= 79) {
+                sonuc.setText(isonuc.toString() + "\n" + "Harf Notu: CB")
+            } else if (80 <= isonuc && isonuc <= 84) {
+                sonuc.setText(isonuc.toString() + "\n" + "Harf Notu: BB")
+            } else if (85 <= isonuc && isonuc <= 89) {
+                sonuc.setText(isonuc.toString() + "\n" + "Harf Notu: BA")
+            } else if (90 <= isonuc && isonuc <= 100) {
+                sonuc.setText(isonuc.toString() + "\n" + "Harf Notu: DE")
+            }
+        }}}
